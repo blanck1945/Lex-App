8018705f4e47673e14fd3da74af1fb59c5f28df0
Okubo <aspastrana990@gmail.com>
2020-10-20 17:23:09 -0300

    Beta

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -9,53 +9,54 @@ import cookie from "cookie";
 dbConnect();
 
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req;
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST", "PUT"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 
-  const erroMsg = "Algo fallo en la autenticación";
-  const succMsg = "Usuario validado con exito";
+  try {
+    const erroMsg = "Algo fallo en la autenticación";
+    const succMsg = "Usuario validado con exito";
 
-  switch (method) {
-    case "POST":
-      const registerUser = await Models.UsuarioModel.findOne({
-        usuario: req.body.usuario,
+    const registerUser = await Models.UsuarioModel.findOne({
+      usuario: req.body.usuario,
+    });
+    if (!registerUser) {
+      res.status(400).json({
+        msg: erroMsg,
       });
-      if (!registerUser) {
-        res.status(400).json({
-          msg: erroMsg,
+    }
+
+    await compare(req.body.password, registerUser.password, async function (
+      err,
+      result
+    ) {
+      if (!err && result) {
+        const token = sign(
+          { userID: registerUser._id, userEmail: registerUser.email },
+          process.env.JWT_SECRET,
+          { expiresIn: "3h" }
+        );
+
+        res.setHeader(
+          "Set-Cookie",
+          cookie.serialize("authCookie", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            sameSite: "lax",
+            maxAge: 3600 * 3,
+            path: "/",
+          })
+        );
+        res.end({
+          succes: true,
+          msg: succMsg,
         });
       }
-
-      await compare(req.body.password, registerUser.password, async function (
-        err,
-        result
-      ) {
-        if (!err && result) {
-          const token = sign(
-            { userID: registerUser._id, userEmail: registerUser.email },
-            process.env.JWT_SECRET,
-            { expiresIn: "3h" }
-          );
-
-          res.setHeader(
-            "Set-Cookie",
-            cookie.serialize("authCookie", token, {
-              httpOnly: true,
-              secure: process.env.NODE_ENV !== "development" ? true : false,
-              sameSite: "strict",
-              maxAge: 3600 * 3,
-              path: "/",
-            })
-          );
-          res.json({
-            succes: true,
-            msg: succMsg,
-          });
-        }
-      });
-      break;
-    default:
-      res.setHeader("Allow", ["POST", "PUT"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).end(error.message);
   }
 }
 
