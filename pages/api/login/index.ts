e008027653089bb2f54ee21ce3efcf348551ffcf
Okubo <aspastrana990@gmail.com>
2020-10-20 12:48:06 -0300

    Beta

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -1,52 +1,52 @@
 import dbConnect from "../../../db/dbConnect";
 import { Models } from "../../../models";
 import { getRes } from "../../../util/funcs";
-import {compare} from "bcrypt"
+import { compare } from "bcrypt";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { sign } from "jsonwebtoken";
-import cookie from "cookie"
+import cookie from "cookie";
 
 dbConnect();
 
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
 
+  const erroMsg = "Algo fallo en la autenticación";
+  const succMsg = "Usuario validado con exito";
 
-export const config = {
-    api: {
-      externalResolver: true,
-      bodyParser: true
-    },
+  if (method === "POST") {
+    const registerUser = await Models.UsuarioModel.findOne({
+      usuario: req.body.usuario,
+    });
+    if (!registerUser) {
+      res.status(400).json({
+        msg: erroMsg,
+      });
+    }
+
+    await compare(req.body.password, registerUser.password);
+    const token = sign(
+      { userID: registerUser._id, userEmail: registerUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "3h" }
+    );
+    return new Promise((resolve, rejex) => {
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("authCookie", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          sameSite: "strict",
+          maxAge: 3600 * 3,
+          path: "/",
+        })
+      );
+      res.status(200).json({ response: { ...getRes(succMsg) } });
+      res.end();
+      return resolve();
+    });
+    //res.status(404).json(getRes(erroMsg))
+  } else {
+    res.status(400).json({ msg: "No Request" });
   }
-
-export default async (req:NextApiRequest, res: NextApiResponse) => {
-    const {method} = req
-
-
-    const erroMsg = "Algo fallo en la autenticación"
-    const succMsg = "Usuario validado con exito"
-
-    if(method === "POST"){
-        console.log("using this route")
-                const registerUser = await Models.UsuarioModel.findOne({
-                    usuario: req.body.usuario
-                })
-                if(!registerUser){
-                    res.status(400).json({
-                        msg: erroMsg
-                    })
-                }
-                
-            await compare(req.body.password, registerUser.password)
-            const token = sign({ userID: registerUser._id, userEmail: registerUser.email  }, process.env.JWT_SECRET, {expiresIn: "3h"});
-            res.setHeader("Set-Cookie", cookie.serialize("authCookie", token, {
-                httpOnly: true,
-                            secure: process.env.NODE_ENV !== "development" ,
-                            sameSite: "strict",
-                            maxAge: 3600 * 3,
-                            path: "/"
-                        }))
-                        res.status(200).json({response: {...getRes(succMsg)}})
-                        res.end()
-                        //res.status(404).json(getRes(erroMsg))
-                    }
-                
-                    }
+};
