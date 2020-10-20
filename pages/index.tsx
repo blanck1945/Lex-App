878b45c56aee1934895b5abe9b78889d42cacb5e
Okubo <aspastrana990@gmail.com>
2020-10-20 14:15:28 -0300

    Beta

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ const Login = () => {
 
       return true;
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
       console.log("Process end");
       setLoading(false);
     }
