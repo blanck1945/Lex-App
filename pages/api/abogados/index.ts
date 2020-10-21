132a7261876ab17b535b87d32f25f6150c07cc2b
Okubo <aspastrana990@gmail.com>
2020-10-21 00:01:04 -0300

    Beta

--- pages/api/abogados/index.ts
+++ pages/api/abogados/index.ts
@@ -7,26 +7,23 @@ import { AutenthicateUser } from "../middleware/Auth";
 dbConnect();
 
 export default async (req, res) => {
-    const {method} = req
+  const { method } = req;
 
-    switch(method){
-        case "GET":
-            try{
-                const abogados = await Abogado.find({})
+  switch (method) {
+    case "GET":
+      const abogados = await Abogado.find({});
 
-                res.status(200).json(getRes(abogados))
-            }
-            catch(err){
-                res.status(400).json(getRes(err))
-            }
-            break;
-            case "POST":
-                    Models.AbogadoModel.create(req.body)
-                    .then(abogado => res.status(201).json({msg: "Abogado creado con exito"}) )
-                    .catch(err =>console.log(err)) 
-                break;
-                default:
-                    res.status(400).json({ msg: "no Match" });
-                    break;
-    }
-}
+      res.status(200).json(getRes(abogados));
+      break;
+    case "POST":
+      Models.AbogadoModel.create(req.body)
+        .then((abogado) =>
+          res.status(201).json({ msg: "Abogado creado con exito" })
+        )
+        .catch((err) => console.log(err));
+      break;
+    default:
+      res.status(400).json({ msg: "no Match" });
+      break;
+  }
+};
