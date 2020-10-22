c37f831adafedcd2137e53af6a3c2b27e192b403
Okubo <aspastrana990@gmail.com>
2020-10-21 23:58:35 -0300

    Disploy

--- src/pages/abogados/index.tsx
+++ src/pages/abogados/index.tsx
@@ -19,7 +19,7 @@ interface AbogadosProps {
 
 const Abogados = ({ abogados }: AbogadosProps) => {
   const router = useRouter();
-  const { data: abogadosArr, error } = useSWR(prefix + "abogados", AxiosFetch);
+  const { data: abogadosArr, error } = useSWR("/api/abogados", AxiosFetch);
   if (error) return <SWRHandler control="error" />;
   if (!abogadosArr) return <SWRHandler control="succ" />;
 
